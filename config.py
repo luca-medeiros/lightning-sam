25a01ad5534004fde0ad9727165b0cea7941a19f
luca-medeiros <luca.medeiros@nuvi-labs.com>
2023-04-22 08:32:38 +0000

    add config

---
+++ config.py
@@ -0,0 +1,35 @@
+from box import Box
+
+config = {
+    "num_devices": 4,
+    "batch_size": 8,
+    "num_workers": 4,
+    "num_epochs": 300,
+    "eval_interval": 3,
+    "out_dir": "out/training",
+    "opt": {
+        "learning_rate": 1e-4,
+        "weight_decay": 0,
+    },
+    "model": {
+        "type": 'vit_h',
+        "checkpoint": "sam_vit_h_4b8939.pth",
+        "freeze": {
+            "image_encoder": True,
+            "prompt_encoder": True,
+            "mask_decoder": False,
+        },
+    },
+    "dataset": {
+        "train": {
+            "root_dir": "/mnt/nas/data/coco/coco2017/train2017",
+            "annotation_file": "/mnt/nas/data/coco/coco2017/annotations/instances_train2017.json"
+        },
+        "val": {
+            "root_dir": "/mnt/nas/data/coco/coco2017/val2017",
+            "annotation_file": "/mnt/nas/data/coco/coco2017/annotations/instances_val2017.json"
+        }
+    }
+}
+
+cfg = Box(config)
